6ec2b3249906d28bc6e917b2b31a2bf6061409ad
chkalov12 <chkalov007@gmail.com>
2020-11-02 23:21:56 +0200

    add slider

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.slider').slick({
+        autoplay: true,
+        fade: true,
+        speed: 1000,
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/left.png" alt=""></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>'
+    });
+
+
+
+});

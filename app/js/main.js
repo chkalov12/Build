5655099c9b124214124ae12d2c609127ec77a3af
chkalov12 <chkalov007@gmail.com>
2020-11-05 22:49:50 +0200

    add headernav

--- app/js/main.js
+++ app/js/main.js
@@ -4,8 +4,9 @@ $(function(){
         autoplay: true,
         fade: true,
         speed: 1000,
-        prevArrow: '<button class="slick-btn slick-prev"><img src="images/left.png" alt=""></button>',
-        nextArrow: '<button class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>'
+        // prevArrow: '<button class="slick-btn slick-prev"><img src="images/left.png" alt=""></button>',
+        // nextArrow: '<button class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>'
+        arrows: false
     });
 
 

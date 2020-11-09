e2418d9c4e7d300a37504948b2ebd35446decfdc
chkalov12 <chkalov007@gmail.com>
2020-11-09 21:48:34 +0200

    add forms

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
 
     $('.slider').slick({
         autoplay: true,
@@ -9,6 +9,26 @@ $(function(){
         arrows: false
     });
 
+    $(".about__range-cost").ionRangeSlider({
+        type: "double",
+        min: 483000,
+        max: 1256395,
+        from: 483000,
+        to: 1256395,
+        prefix: "$"
+    });
 
+    let room = document.querySelectorAll('.about__rooms-btn');
+    for (i = 0; i < room.length; i++) {
+        room[i].onclick = show;
+    }
+
+    function show() {
+        let room = document.querySelectorAll('.about__rooms-btn');
+        for (i = 0; i < room.length; i++) {
+            room[i].classList.remove('about__rooms-btn--active');
+            this.classList.add('about__rooms-btn--active');
+        }
+    }
 
 });

3278c56db9cb01d012b1a8da64da932fbb344bcc
chkalov12 <chkalov007@gmail.com>
2020-11-10 20:50:40 +0200

    do form

--- app/js/main.js
+++ app/js/main.js
@@ -9,13 +9,64 @@ $(function() {
         arrows: false
     });
 
-    $(".about__range-cost").ionRangeSlider({
+    let $range = $(".about__range-cost");
+    let $result = $(".about__result-cost");
+
+    function prettify(num) {
+        var n = num.toString();
+        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + " ");
+    }
+
+    $range.ionRangeSlider({
         type: "double",
+        skin: "round",
         min: 483000,
         max: 1256395,
         from: 483000,
         to: 1256395,
-        prefix: "$"
+        force_edges: false,
+        hide_min_max: true,
+        hide_from_to: true,
+        prettify_separator: " ",
+        onStart: function(data) {
+            $result.text(" $ " + prettify(data.from) + " — " + " " + " $ " + data.to_pretty);
+        },
+        onChange: function(data) {
+            $result.text(" $ " + prettify(data.from) + " — " + " " + " $ " + data.to_pretty);
+        }
+    });
+
+    $(".about__range-yard").ionRangeSlider({
+        type: "double",
+        skin: "round",
+        min: 483000,
+        max: 1256395,
+        from: 483000,
+        to: 1256395,
+        prefix: "",
+        grid: false,
+        force_edges: false,
+        hide_min_max: true,
+        hide_from_to: true,
+        block: false
+
+    });
+
+    $(".about__range-floor").ionRangeSlider({
+        type: "double",
+        skin: "round",
+        min: 483000,
+        max: 1256395,
+        from: 483000,
+        to: 1256395,
+        prefix: "",
+        grid: false,
+        force_edges: false,
+        hide_min_max: true,
+        hide_from_to: true,
+        block: false,
+        step: 1
+
     });
 
     let room = document.querySelectorAll('.about__rooms-btn');

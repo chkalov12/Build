651ea05ba831e326b560abc228234c149d76accd
chkalov12 <chkalov007@gmail.com>
2020-11-11 20:44:14 +0200

    form complete

--- app/js/main.js
+++ app/js/main.js
@@ -36,37 +36,92 @@ $(function() {
         }
     });
 
-    $(".about__range-yard").ionRangeSlider({
+    // $(".about__range-yard").ionRangeSlider({
+    //     type: "double",
+    //     skin: "round",
+    //     min: 483000,
+    //     max: 1256395,
+    //     from: 483000,
+    //     to: 1256395,
+    //     prefix: "",
+    //     grid: false,
+    //     force_edges: false,
+    //     hide_min_max: true,
+    //     hide_from_to: true,
+    //     block: false
+
+    // });
+
+    let $yard = $(".about__range-yard");
+    let $resultyard = $(".about__result-yard");
+
+    function prettify(num) {
+        var n = num.toString();
+        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + " ");
+    }
+
+    $yard.ionRangeSlider({
         type: "double",
         skin: "round",
-        min: 483000,
-        max: 1256395,
-        from: 483000,
-        to: 1256395,
-        prefix: "",
-        grid: false,
+        min: 70,
+        max: 236,
+        from: 70,
+        to: 236,
         force_edges: false,
         hide_min_max: true,
         hide_from_to: true,
-        block: false
-
+        prettify_separator: " ",
+        onStart: function(data) {
+            $resultyard.text(prettify(data.from) + " — " + data.to_pretty);
+        },
+        onChange: function(data) {
+            $resultyard.text(prettify(data.from) + " — " + data.to_pretty);
+        }
     });
 
-    $(".about__range-floor").ionRangeSlider({
+    // $(".about__range-floor").ionRangeSlider({
+    //     type: "double",
+    //     skin: "round",
+    //     min: 483000,
+    //     max: 1256395,
+    //     from: 483000,
+    //     to: 1256395,
+    //     prefix: "",
+    //     grid: false,
+    //     force_edges: false,
+    //     hide_min_max: true,
+    //     hide_from_to: true,
+    //     block: false,
+    //     step: 1
+
+    // });
+
+    let $floor = $(".about__range-floor");
+    let $resultfloor = $(".about__result-floor");
+
+    function prettify(num) {
+        var n = num.toString();
+        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + " ");
+    }
+
+    $floor.ionRangeSlider({
         type: "double",
         skin: "round",
-        min: 483000,
-        max: 1256395,
-        from: 483000,
-        to: 1256395,
-        prefix: "",
-        grid: false,
+        min: 1,
+        max: 4,
+        from: 1,
+        to: 4,
         force_edges: false,
         hide_min_max: true,
         hide_from_to: true,
-        block: false,
-        step: 1
-
+        step: 1,
+        prettify_separator: " ",
+        onStart: function(data) {
+            $resultfloor.text(prettify(data.from) + " — " + data.to_pretty);
+        },
+        onChange: function(data) {
+            $resultfloor.text(prettify(data.from) + " — " + data.to_pretty);
+        }
     });
 
     let room = document.querySelectorAll('.about__rooms-btn');
